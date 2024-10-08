e6317e9d60e6c6e1801fd68833511dfd706a0167
Di Ta <xditaba@gmail.com>
2024-10-08 17:18:18 +0700

    All

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-// import { UserProfile } from "./One/UserProfile";
-// import { Collection } from "./Three/Collection";
-// import { JobList } from "./Two/JobList";
-// import { ReviewForm } from "./Four/ReviewForm";
-// import { UsersList } from "./Five/UsersList";
-// import { Notification } from "./Six/Notification";
-// import { Subscriptions } from "./Seven/Subscriptions";
-// import { JobPositions } from "./Eight/JobPositions";
-
+import { UserProfile } from "./One/UserProfile";
+import { Collection } from "./Three/Collection";
+import { JobList } from "./Two/JobList";
+import { ReviewForm } from "./Four/ReviewForm";
+import { UsersList } from "./Five/UsersList";
+import { Notification } from "./Six/Notification";
+import { Subscriptions } from "./Seven/Subscriptions";
+import { JobPositions } from "./Eight/JobPositions";
 import { AccountList } from "./Nine/AccountList";
 
 function App() {
   return (
-    <div className="flex h-full w-full items-center justify-center bg-[#e6effa] p-20">
-      {/* <UserProfile /> */}
-      {/* <JobList /> */}
-      {/* <Collection /> */}
-      {/* <ReviewForm /> */}
-      {/* <UsersList /> */}
-      {/* <Notification /> */}
-      {/* <Subscriptions /> */}
-      {/* <JobPositions /> */}
-      <AccountList />
+    <div className="flex flex-col items-center justify-center gap-y-4">
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <UserProfile />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <JobList />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <Collection />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <ReviewForm />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <UsersList />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <Notification />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <Subscriptions />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <JobPositions />
+      </div>
+      <div className="flex flex-col items-center justify-center bg-[#e6effa] p-20">
+        <AccountList />
+      </div>
     </div>
   );
 }
